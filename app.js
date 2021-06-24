0b2456ef46cde46976796d4af35ae83d74eea0d7
WebsiteOenO <86375422+WebsiteOenO@users.noreply.github.com>
2021-06-24 20:26:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,20 @@ function scrollWin() {
         behavior: 'smooth'
     });
 }
+
+function menubar() {
+  const navbar = document.querySelector(".navbar");
+  const menu = document.querySelector(".menu-list")
+  const menuBtn = document.querySelector(".menu-btn");
+  const cancelBtn = document.querySelector(".cancel-btn");
+  menuBtn.onclick = ()=>{
+    menu.classList.add("active");
+    menuBtn.classList.add("hide");
+  };
+  cancelBtn.onclick = ()=>{
+    menu.classList.remove("active");
+    menuBtn.classList.remove("hide");
+  };
+}
+
+menubar();
